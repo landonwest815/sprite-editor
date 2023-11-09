6e90bfd37a562d41701945d77af65a40c33ef660
Christian Hartman <122404963+Is-This-One-Avaiable@users.noreply.github.com>
2023-11-09 12:51:36 -0700

    Added Tool and Color functionality to Model class

--- model.h
+++ model.h
@@ -4,36 +4,28 @@
 #include "Tool.h"
 #include <QColor>
 #include <string>
+#include <vector>
 
 class Model
 {
 private:
-    Tool draw;
-    Tool erase;
-    Tool mirror;
+    std::vector<Tool> toolBar;
     QColor selectedColor;
 
 public:
     ///@brief Blank constructor for the Model class. Sets the selected tool to the brush and selected color to black.
     Model();
-    ///@brief Constructor for the model class with parameters for the selected tool and color.
-    /// @param selectedTool - Tool to be selected once the class is initialized.
-    /// @param selectedColor - Color to be selected once the class is initialized.
-    Model(Tool selectedTool, QColor selectedColor);
 
-    ///@brief setter for the currently selected tool
-    ///@param toolName - name of the tool to be selected
-    void setSelectedTool(std::string toolName);
-    ///@brief getter for the currrently selected tool
-    ///@return - The currently selected tool
-    Tool getSelectedTool();
+    ///@brief getter for all of the tools
+    ///@return - Dynamic list (vector) of all of the tools
+    std::vector<Tool> getTools();
 
     ///@brief setter for the currently selected color
     ///@param color - color to be selected
     void setSelectedColor(QColor color);
     ///@brief getter for the currrently selected color
     ///@return - The currently selected color
-    Tool getSelectedColor();
+    QColor getSelectedColor();
 };
 
 #endif // MODEL_H
