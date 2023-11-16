70be84641b0766cd1bc11d1b0380f6a9b3a79166
Taizuki <ryan.tat.nguyen@gmail.com>
2023-11-15 16:18:22 -0800

    Adjustments

--- filemanager.h
+++ filemanager.h
@@ -1,14 +1,3 @@
-#ifndef FILEMANAGER_H
-#define FILEMANAGER_H
-#include <QJsonObject>
-#include <QJsonArray>
-#include <QJsonDocument>
-#include <QFileDialog>
-#include <QMessageBox>
-#include <cmath>
-#include "frame.h"
-#include "model.h"
-
 /**
  * Author(s):     Team Geoff:
  *                Ryan Nguyen (Taizuki), John Nguyen (johnguyn),
@@ -22,42 +11,51 @@
  * file. It assists in declarations of the FileManager object, functions, etc.
  */
 
-/**
- * @class FileManager
- * @brief The FileManager class represents the save and load features of the sprite editor.
- * It helps to take a vector of Frame objects and save it as Json within a sprite file, and
- * it also assists in loading a sprite file by converting it back to a vector of Frames.
- */
-class FileManager : public QObject
-{
-    Q_OBJECT
+#ifndef FILEMANAGER_H
+#define FILEMANAGER_H
 
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QJsonDocument>
+#include <QFileDialog>
+#include <QMessageBox>
+#include <cmath>
+#include "frame.h"
+#include "model.h"
+
+/**
+ * @brief The FileManager class handles the import and export of frames in JSON format.
+ * It uses a QJsonObject to store JSON data and communicates with the Model class to perform operations.
+ */
+class FileManager : public QObject {
+    Q_OBJECT
 private:
-    /// @brief Object in which Json is written into
+    /// @brief The QJsonObject used to write JSON data.
     QJsonObject writeJson;
 
-    /// @brief Model that is utilized for color
+    /// @brief The Model object used to interact with frame data.
     Model model;
 
 public:
-    /// @brief Constructor for the FileManager
+    /// @brief Constructor for the FileManager class.
+    /// @param parent The parent QObject.
     FileManager(QObject *parent = nullptr);
 
 public slots:
-    /// @brief Converts vector of Frames into Json stored in a sprite file
-    /// @param vector<Frame> Vector of Frames which will be written into a sprite file
-    /// @param QString The path where the sprite file will be saved
-    void exportJson(std::vector<Frame>, QString);
+    /// @brief Export frames to a JSON file.
+    /// @param frames The vector of frames to export.
+    /// @param filePath The file path to save the JSON file.
+    void exportJson(std::vector<Frame> frames, QString filePath);
 
-    /// @brief Converts Json within sprite file into vector of Frames
-    /// @param QString The path where the sprite file will be loaded
-    /// @return Vector of Frames that contains the new data from loaded sprite file
-    std::vector<Frame> importJson(QString);
+    /// @brief Import frames from a JSON file.
+    /// @param filePath The file path to the JSON file.
+    /// @return The vector of frames imported from the JSON file.
+    std::vector<Frame> importJson(QString filePath);
 
 signals:
-    /// @brief Signal that emits when any error occurs saving or loading
-    /// @param QString Error message
-    void errorMessage(QString);
+    /// @brief Signal emitted when an error message needs to be displayed.
+    /// @param message The error message to display.
+    void errorMessage(QString message);
 };
 
 #endif // FILEMANAGER_H
